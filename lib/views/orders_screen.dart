cb47d9fccc691d80e12587c0cec60e807c72d37d
PhRezende-eng <ph.hr.001@gmail.com>
2021-07-10 00:41:03 -0300

    add a Future Builder in orders screen, so i remove the init state and ternary

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../widgets/app_drawer.dart';
 import '../providers/orders.dart';
 import '../widgets/order_widget.dart';
@@ -11,40 +10,55 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  bool isLoading = true;
-  @override
-  void initState() {
-    Provider.of<Orders>(context, listen: false).loadOrder().then((_) {
-      setState(() {
-        isLoading = false;
-      });
-    });
-    super.initState();
-  }
+  // bool isLoading = true;
+  // @override
+  // void initState() {
+  //   // Provider.of<Orders>(context, listen: false).loadOrder().then((_) {    -> não precisa mais pois tiramos o ternário e o init state é no Future Builder
+  //   //   setState(() {
+  //   //     isLoading = false;
+  //   //   });
+  //   // });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final Orders orders = Provider.of(context);
+    // final Orders orders = Provider.of(context);
     // final orders = Provider.of<Orders>(context);
+    final ordersPro = Provider.of<Orders>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
-      body: isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: ordersPro.loadOrder(),
+        builder: (contextt, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: () {
-                return _loadScreen(context);
+            );
+          } else if (snapshot.error != null) {
+            return Center(
+              child: Text('Ocorreu um erro!'),
+            );
+          } else {
+            return Consumer<Orders>(
+              builder: (context, ordersBui, child) {
+                return RefreshIndicator(
+                  onRefresh: () => _loadScreen(context),
+                  child: ListView.builder(
+                    itemCount: ordersBui.itemsCount,
+                    itemBuilder: (ctx, index) =>
+                        OrderWidget(ordersBui.items[index]),
+                  ),
+                );
               },
-              child: ListView.builder(
-                itemCount: orders.itemsCount,
-                itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
-              ),
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 
